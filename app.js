bbdb09503674495d514043f43be04942c73d6ec0
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-04-17 03:27:30 -0400

    succesful image upload feature using malter

--- app.js
+++ app.js
@@ -27,9 +27,64 @@ app.get('/upload', (req, res)=> {
 });
 
 app.get('/', (req,res) => {
-    res.render('index')
+    Picture.find({})
+        .then(images => {
+            res.render('index' , {images : images})
+        });
 })
 
+//Set Image Storage
+let storage = multer.diskStorage({
+    destination: './public/uploads/images',
+    filename : (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+});
+
+let upload = multer({
+    storage : storage,
+    fileFilter : (req, file, cb) => {
+        checkFileType(file, cb)
+    }
+});
+
+function checkFileType(file, cb) {
+    const fileTypes = /jpeg|jpg|png|gif/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+    if(extname){
+        return cb(null, true)
+    } else {
+        cb('Error: Please upload images only')
+    }
+}
+
+app.post('/uploadsingle', upload.single('singleImage'), (req, res, next)=> {
+    const file = req.file;
+    if(!file) {
+        return console.log('Please select an Image')
+    }
+
+    console.log(file.path);
+    let url = file.path.replace('public', '');
+
+    Picture.findOne({imgUrl : url})
+        .then( img => {
+            if(img) {
+                console.log('Duplicate Image. Try again!')
+                return res.redirect('/upload');
+            }
+
+            Picture.create({imgUrl : url})
+                .then(img => {
+                    console.log('Image saved to DB.')
+                    res.redirect('/')
+                })
+        })
+        .catch(err => {
+            return console.log('ERROR:' +err)
+        });
+});
 
 app.listen(3000, ()=> {
     console.log('Server has started')
